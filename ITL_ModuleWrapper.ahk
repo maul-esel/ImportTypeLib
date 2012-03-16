maul-esel/ImportTypeLib

commit 7d765d84efcb5fb25e5d652e3260a01b72f28474

begin TI_ModuleWrapper.__Call()

ITL_ModuleWrapper.ahk
@@ -13,5 +13,27 @@ class ITL_ModuleWrapper extends ITL_Wrapper.ITL_ConstantMemberWrapperBaseClass
 
 	__Call(method, params*)
 	{
+		static DISPID_UNKNOWN := -1
+		local id := DISPID_UNKNOWN, hr := 0, addr := 0, info
+
+		info := this["internal://typeinfo-instance"]
+
+		hr := DllCall(NumGet(NumGet(info+0), 10*A_PtrSize, "Ptr"), "Ptr", info, "Str*", method, "UInt", 1, "Ptr*", id, "Int")
+		if (FAILED(hr) || id == DISPID_UNKNOWN)
+		{
+			throw Exception("GetIDsOfNames() for """ method "()"" failed.", -1, ITL_FormatError(hr))
+		}
+
+		hr := DllCall(NumGet(NumGet(info+0), 15*A_PtrSize, "Ptr"), "Ptr", info, "UInt", id, "UInt", 0, "Ptr*", addr, "Int")
+		if (FAILED(hr) || !addr)
+		{
+			throw Exception("AddressOfMember() for """ method "()"" failed.", -1, ITL_FormatError(hr))
+		}
+
+		; call!
+		; result := DllCall(addr, params*)
+		; get return type
+
+		return ; result
 	}
 }

commit 2a7033e976bd30f53e574728d1fa781413e040f0

fix function names (broken in commit 7d765d84ef)

ITL_ModuleWrapper.ahk
@@ -19,13 +19,13 @@ class ITL_ModuleWrapper extends ITL_Wrapper.ITL_ConstantMemberWrapperBaseClass
 		info := this["internal://typeinfo-instance"]
 
 		hr := DllCall(NumGet(NumGet(info+0), 10*A_PtrSize, "Ptr"), "Ptr", info, "Str*", method, "UInt", 1, "Ptr*", id, "Int")
-		if (FAILED(hr) || id == DISPID_UNKNOWN)
+		if (ITL_FAILED(hr) || id == DISPID_UNKNOWN)
 		{
 			throw Exception("GetIDsOfNames() for """ method "()"" failed.", -1, ITL_FormatError(hr))
 		}
 
 		hr := DllCall(NumGet(NumGet(info+0), 15*A_PtrSize, "Ptr"), "Ptr", info, "UInt", id, "UInt", 0, "Ptr*", addr, "Int")
-		if (FAILED(hr) || !addr)
+		if (ITL_FAILED(hr) || !addr)
 		{
 			throw Exception("AddressOfMember() for """ method "()"" failed.", -1, ITL_FormatError(hr))
 		}